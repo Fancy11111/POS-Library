bc04726c44e8452c97c582a1fae81407cbd46df5
Daniel Fenz <dani.fenz@aon.at>
2019-03-27 23:30:30 +0100

    Added propositional logic

---
+++ PropositionalLogic/AbstractExpression.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropositionalLogic
+{
+    public abstract class AbstractExpression
+    {
+        public abstract void ParseFormel(List<char> formel);
+
+        //Variablenliste wird beim Parsen befüllt
+        public static List<char> variables = new List<char>();
+
+        public abstract bool Interpret(Dictionary<char, bool> context);
+    }
+}
---
+++ PropositionalLogic/VariableExpression.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropositionalLogic
+{
+    public class VariableExpression : AbstractExpression
+    {
+        private char varName;
+        public override bool Interpret(Dictionary<char, bool> context)
+        {
+            return context[varName];
+        }
+
+        public override void ParseFormel(List<char> formel)
+        {
+            if (formel.First() < 'a' || formel.First() > 'z'){
+                throw new Exception("Variable must be between a and z");
+            }
+            else
+            {
+                varName = formel.First();
+                if (!AbstractExpression.variables.Contains(varName))
+                {
+                    AbstractExpression.variables.Add(varName);
+                }
+                formel.RemoveAt(0);
+            }
+            
+        }
+    }
+}
