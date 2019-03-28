1d13db8786eda099f4335edfae83c37deae8e6bd
Daniel Fenz <daniel.fenz.at@gmail.com>
2019-03-28 07:47:36 +0100

    Small fixes to ForeignKeys in DatabaseORM

--- DatabaseORM/Relation.cs
+++ DatabaseORM/Relation.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.Linq.Mapping;
+﻿using System.Data.Linq;
+using System.Data.Linq.Mapping;
 
 namespace DatabaseORM
 {
@@ -11,17 +12,48 @@ namespace DatabaseORM
         [Column(IsPrimaryKey = true)]
         public int? ID { get; set; }
 
-        // This is a foreign key
-        // Look at Database.cs for further details
-        [Column]
-        public int PersonID1 { get; set; }
-
-        // This is a foreign key
-        // Look at Database.cs for further details
-        [Column]
-        public int PersonID2 { get; set; }
 
         [Column]
         public string Status { get; set; }
+
+
+        #region Foreign Keys/Associations
+        // This section is used to allow direct access to the Entity referenced via a foreign key in a table
+        // In this example Relations has to foreign keys, both referencing to a Person
+
+        // This field stores the actual foreign key
+        [Column(Name = "PersonID1")]
+        private int personid1;
+
+        // this is the Reference to the entity
+        // you can get the entity of an EntityRef via the Entity property
+        private EntityRef<Person> _person1 = new EntityRef<Person>();
+
+        // The property is used to capsule the EntityRef object
+        // IsForeignKey = True for all references, Storage = <EntityRefField>, ThisKey = <ForeignKeyField>
+        [Association(Name = "FK_ROUTER", IsForeignKey = true, Storage = "_person1", ThisKey = "personid1")]
+        public Person Person1
+        {
+            get { return _person1.Entity; }
+            set { _person1.Entity = value; }
+        }
+
+        // This field stores the actual foreign key
+        [Column(Name = "PersonID2")]
+        private int personid2;
+
+        // this is the Reference to the entity
+        // you can get the entity of an EntityRef via the Entity property
+        private EntityRef<Person> _person2 = new EntityRef<Person>();
+
+        // The property is used to capsule the EntityRef object
+        // IsForeignKey = True for all references, Storage = <EntityRefField>, ThisKey = <ForeignKeyField>
+        [Association(Name = "FK_ROUTER", IsForeignKey = true, Storage = "_person2", ThisKey = "personid2")]
+        public Person Person2
+        {
+            get { return _person2.Entity; }
+            set { _person2.Entity = value; }
+        }
+        #endregion
     }
 }
