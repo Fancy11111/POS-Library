45dbac7f662c7ce59bab5bc07b209bdb8b127510
Daniel Fenz <dani.fenz@aon.at>
2019-03-30 23:24:11 +0100

    Implented the Factory Method Pattern

---
+++ FactoryMethodPattern/Factory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethodPattern
+{
+    public enum NodeTypes
+    {
+        Dijkstra, AStar
+    }
+    public class Factory
+    {
+        // Factory Method
+        // Creates a different concrete INode depending on the NodeType given as a parameter
+        public static INode GetNode(NodeTypes type)
+        {
+            if(type == NodeTypes.Dijkstra)
+            {
+                return new DijkstraNode();
+            }
+            else if(type == NodeTypes.AStar)
+            {
+                return new AStarNode();
+            }
+            throw new NotSupportedException();
+        }
+    }
+}
---
+++ FactoryMethodPattern/Node.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethodPattern
+{
+    // The interface INode
+    // Defines one function "getDistance" which returns a double
+    public interface INode
+    {
+        double getDistance();
+    }
+
+    // A class which implements the INode interface
+    // Implements the "getDistance" method defined in the INode interface
+    // Created by the DijkstraNodeFactory
+    public class DijkstraNode : INode
+    {
+        public double Distance { get; set; }
+        public double getDistance()
+        {
+            return Distance;
+        }
+    }
+
+    // A class which implements the INode interface
+    // Implements the "getDistance" method defined in the INode interface
+    // Created by the AStarNodeFactory
+    public class AStarNode : INode
+    {
+        public double H { get; set; }
+        public double G { get; set; }
+        public double getDistance()
+        {
+            return H + G;
+        }
+    }
+}
