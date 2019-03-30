2348e40c65f88ddad01c4b1afd1f0941c467f9e1
Daniel Fenz <dani.fenz@aon.at>
2019-03-30 22:59:12 +0100

    Created the AbstractFactory project, added implentation plus documentation

---
+++ AbstractFactory/Example.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    public class Example
+    {
+        static void main()
+        {
+            var algorithm = "dijkstra";
+            INodeFactory factory;
+            if(algorithm == "dijkstra")
+            {
+                factory = new DijkstraNodeFactory();
+            } else
+            {
+                factory = new AStarNodeFactory();
+            }
+
+            INode node = factory.createNode();
+            Console.WriteLine(node.getDistance());
+        }
+    }
+}
---
+++ AbstractFactory/Node.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace AbstractFactory
+{
+    // The interface INode
+    // Defines one function "getDistance" which returns a double
+    public interface INode
+    {
+        double getDistance();
+    }
+
+    // A class which implements the INode interface
+    // Implements the "getDistance" method defined in the INode interface
+    // Created by the DijkstraNodeFactory
+    public class DijkstraNode : INode
+    {
+        public double Distance { get; set; }
+        public double getDistance()
+        {
+            return Distance;
+        }
+    }
+
+    // A class which implements the INode interface
+    // Implements the "getDistance" method defined in the INode interface
+    // Created by the AStarNodeFactory
+    public class AStarNode : INode
+    {
+        public double H { get; set; }
+        public double G { get; set; }
+        public double getDistance()
+        {
+            return H + G;
+        }
+    }
+}
